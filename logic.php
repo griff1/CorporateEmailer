54933151cae6567fe3d09ae418f0b90ea16f331d
griff1 <gpmaster117@gmail.com>
2015-01-26 22:57:18 -0800

    finished pushing emails

--- logic.php
+++ logic.php
@@ -10,8 +10,12 @@ if(isset($_POST['Submit']))
 	array_push($emails, $firstname . "." . $lastname . "@" . $domain);
 
 	array_push($emails, $lastname . "." . $firstname . "@" . $domain);
-	array_push($emails, $firstname . $lastname . "@" . $domain);
-	array_push($emails, $firstname . $lastname . "@" . $domain);
-	array_push($emails, $firstname . $lastname . "@" . $domain);
+	array_push($emails, $firstname . $lastname[0] . "@" . $domain);
+	array_push($emails, $lastname[0] . $firstname . "@" . $domain);
+	array_push($emails, $firstname[0] . $lastname[0] . "@" . $domain);
+	array_push($emails, $firstname . "@" . $domain);
+	array_push($emails, $lastname . "@" . $domain);
+	array_push($emails, $firstname . "_" . $lastname . "@" . $domain);
+
 
 }
