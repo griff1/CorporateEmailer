033b9c0136dad6f5a122f50596c0a25d921544fa
griff1 <gpmaster117@gmail.com>
2015-01-27 00:47:39 -0800

    bug fixes; functional version

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <html>
-	<form action="logic.php" method="POST">
-		<input type="text" name="firstname">
-		<input type="text" name="lastname">
-		<input type="text" name="companyname">
-		<input type="text" name="domain">
+	<form method="POST">
+		<a>A simple form to generate possible email addresses.  If you are trying to find someone's email, enter their information and this form will generate a scattershot of possible addresses and load them into your email client.</a><br><br>
+		First name: <input type="text" name="firstname"><br><br>
+		Last name: <input type="text" name="lastname"><br><br>
+		Company: <input type="text" name="companyname"><br><br>
+		Your company: <input type="text" name="yourco"><br><br>
+		Domain (e.g. google.com): <input type="text" name="domain"><br><br>
+		<input type="radio" name="client" value="gmail" checked>Gmail/Apple Mail<br>
+		<input type="radio" name="client" value="outlook">Outlook<br><br>
 		<input type="submit" value="Submit" name="Submit">
 	</form>
-
+<?php
+	include("logic.php");
+?>
 </html>
--- logic.php
+++ logic.php
@@ -1,11 +1,14 @@
 <?php
 	$firstname = htmlspecialchars($_POST['firstname']);
 	$lastname = htmlspecialchars($_POST['lastname']);
-	$company = htmlspecialchars($_POST['company']);
+	$company = htmlspecialchars($_POST['companyname']);
 	$domain = htmlspecialchars($_POST['domain']);
+	$yourco = htmlspecialchars($_POST['yourco']);
+	$del = $_POST['client'];
 
 	if(isset($_POST['Submit']))
 	{
+
 		$emails = [];
 
 		array_push($emails, $firstname . $lastname . "@" . $domain);
@@ -18,11 +21,21 @@
 		array_push($emails, $lastname . "@" . $domain);
 		array_push($emails, $firstname . "_" . $lastname . "@" . $domain);
 
+		//echo(implode($emails) . "<br></br>");
 		$string_version = "";
-		for($i = 0; i < count($emails); $i++)
+		$delimiter = "; ";
+		if($del == "gmail")
 		{
-			$string_version = $string_version . "; " . $emails[i];
+			$delimiter = ", ";
 		}
-		echo("<a href=\"mailto:" . $string_version . "> Click here </a>");
+		for($i = 1; $i < count($emails); $i++)
+		{
+			if($string_version == "")
+				$string_version .= $emails[$i];
+			else
+				$string_version = $string_version . $delimiter . $emails[$i];
+		}
+		//fopen("http://198.199.105.42/mailto:$emails[0]?bcc=$string_version&subject=$company%20|%20Summit Partners&body=Dear%20$firstname%20$lastname%20of%20$company,");
+		echo("<a href=\"mailto: $emails[0]?bcc=$string_version&subject=$company%20|%20$yourco&body=Dear%20$firstname%20$lastname%20of%20$company,\"> Click here </a>");
 	}
 ?>
